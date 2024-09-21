264933605b6c9c5e92e31dd71399199a59a64119
Trong Dat <datnt700@gmail.com>
2024-09-21 11:22:45 +0200

    feat: translate app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,96 @@
+'use client';
+import Image from 'next/image';
+import { useEffect, useMemo, useState } from 'react';
+
+import styles from './home.module.scss';
+
+import { InputSection } from '@/components/InputSection';
+import { ResultSection } from '@/components/ResultSection';
+
 export default function Home() {
-  return <div>hello</div>;
+  const languageOption = useMemo(
+    () => [
+      { label: 'English', id: 'en', default: true, active: false },
+      { label: 'French', id: 'fr', default: true, active: false },
+      { label: 'Spanish', id: 'es', default: false, active: false },
+      { label: 'German', id: 'de', default: false, active: false },
+      { label: 'Portuguese', id: 'pt', default: false, active: false },
+    ],
+    []
+  );
+
+  const [text, setText] = useState('Hello, how are you');
+  const [language, setLanguage] = useState(
+    languageOption.map((item) =>
+      item.id === 'en' ? { ...item, active: true } : { ...item, active: false }
+    )
+  );
+  const [secondLanguage, setSecondLanguage] = useState(
+    languageOption.map((item) =>
+      item.id === 'fr' ? { ...item, active: true } : { ...item, active: false }
+    )
+  );
+  const [textResult, setTextResult] = useState('Bonjour, comment allez-vous');
+  const handleSwap = () => {
+    setLanguage(secondLanguage);
+    setSecondLanguage(language);
+    setText(textResult);
+    setTextResult(text);
+  };
+  const translateText = async () => {
+    const translateLanguage = language.find((item) => item.active)?.id;
+    const resultLanguage = secondLanguage.find((item) => item.active)?.id;
+    const languages = `${translateLanguage}|${resultLanguage}`;
+
+    try {
+      const response = await fetch(
+        `https://api.mymemory.translated.net?langpair=${languages}&q=${text}`
+      );
+      const result = await response.json();
+      const newText = result.responseData.translatedText.replace(/"/g, '');
+
+      setTextResult(newText);
+    } catch (error) {
+      console.error('Error translating text:', error);
+    }
+  };
+
+  const handleTranslate = () => {
+    translateText();
+  };
+  return (
+    <div className={styles.contain}>
+      <div className={styles.img}>
+        <Image
+          src={'/images/hero_img.jpg'}
+          alt={'logo'}
+          width={200}
+          height={100}
+          className={styles['background-image']}
+        />
+        <Image
+          src={'/images/logo.svg'}
+          alt={'logo'}
+          width={200}
+          height={100}
+          className={styles.logo}
+        />
+      </div>
+      <div className={styles.content}>
+        <InputSection
+          text={text}
+          setText={setText}
+          handleTranslate={handleTranslate}
+          languageOption={language}
+          setLanguageOption={setLanguage}
+        />
+        <ResultSection
+          textResult={textResult}
+          languageOption={secondLanguage}
+          setLanguageOption={setSecondLanguage}
+          handleSwap={handleSwap}
+        />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ResultSection/index.tsx
@@ -0,0 +1,136 @@
+import Image from 'next/image';
+import { useState } from 'react';
+import { Icon } from '@iconify/react';
+
+import styles from '../styles.module.scss';
+
+import { FeatureSection } from '@/components/FeatureSection';
+
+interface LanguageOption {
+  label: string;
+  id: string;
+  default: boolean;
+  active: boolean;
+}
+interface ResultSectionProps {
+  handleSwap: () => void;
+  textResult: string;
+  languageOption: LanguageOption[];
+  setLanguageOption: (options: LanguageOption[]) => void;
+}
+
+export const ResultSection = ({
+  textResult,
+  handleSwap,
+  languageOption,
+  setLanguageOption,
+}: ResultSectionProps) => {
+  const [activeList, setActiveList] = useState(false);
+
+  const handleLanguage = (lang) => {
+    setActiveList(false);
+
+    if (lang.id === languageOption[0].id || lang.id === languageOption[1].id) {
+      const updated = languageOption.map((item) =>
+        item.id === lang.id
+          ? { ...item, active: true }
+          : { ...item, active: false }
+      );
+      setLanguageOption(updated);
+    } else {
+      const updatedOptions = languageOption.map((item) =>
+        item.id === lang.id
+          ? { ...item, default: true, active: true }
+          : { ...item, default: false, active: false }
+      );
+      const selectedLang = updatedOptions.find((item) => item.default);
+      const reorderedLanguages = selectedLang
+        ? [selectedLang, ...updatedOptions.filter((item) => !item.default)]
+        : [...updatedOptions];
+
+      setLanguageOption(reorderedLanguages);
+    }
+  };
+  const handleList = () => {
+    setActiveList((e) => !e);
+  };
+
+  function handleSound() {
+    const message = new SpeechSynthesisUtterance(textResult);
+    message.lang = languageOption.find((item) => item.active && !item.default)
+      ?.id as string;
+    speechSynthesis.speak(message);
+  }
+
+  function handleCopy() {
+    navigator.clipboard.writeText(textResult);
+  }
+
+  return (
+    <div className={styles['input-section']}>
+      <div className={styles.tabs}>
+        <div className={styles.wrapper}>
+          <ul className={styles['tab-list']}>
+            {languageOption.slice(0, 2).map((lang) => (
+              <li key={lang.id}>
+                <button
+                  onClick={() => handleLanguage(lang)}
+                  className={`${styles.tab} ${lang.active && styles.activeTab}`}
+                >
+                  {lang.label}
+                </button>
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles.list}>
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */}
+            <div className={styles.selected} onClick={handleList}>
+              <span className={styles.btn}>
+                Select More
+                {activeList ? (
+                  <Icon icon="ri:arrow-down-s-line" />
+                ) : (
+                  <Icon icon="solar:alt-arrow-up-linear" />
+                )}
+              </span>
+            </div>
+            <div
+              className={
+                activeList ? `${styles.menu} ${styles.activeList}` : styles.menu
+              }
+            >
+              <ul>
+                {languageOption.slice(2).map((lang) => (
+                  <li key={lang.id}>
+                    <button onClick={() => handleLanguage(lang)}>
+                      {lang.label}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <button className={styles.icon} onClick={handleSwap}>
+          <Image
+            src={'images/Horizontal_top_left_main.svg'}
+            alt={'img'}
+            width={20}
+            height={20}
+          />
+        </button>
+      </div>
+      <div className={styles.line}></div>
+      <div className={styles['input-text']}>
+        <textarea className={styles.area} value={textResult}>
+          {textResult}
+        </textarea>
+      </div>
+      <div className={styles.features}>
+        <FeatureSection handleSound={handleSound} handleCopy={handleCopy} />
+      </div>
+    </div>
+  );
+};
